cba0e1eb1d2982ce8cae12278d146c3ee76c9af0
CapUgleplast <kpowdm@yandex.ru>
2024-02-28 17:36:21 +0700

    init

---
+++ src/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter} from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <BrowserRouter>
+
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
+
